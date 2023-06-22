6b670768ef3a7d3bb296c7679c49f931df3fe529
Elgamal <ahmedelgamal5320@gmail.com>
2023-06-22 22:41:35 +0300

    update currentDate(): no need to add 3 hours

--- src/apis/utils/currentDate.js
+++ src/apis/utils/currentDate.js
@@ -2,13 +2,12 @@ const currentDate = () => {
   const now = new Date();
 
   //+3 hour to get cairo timezone with summer time modification
-  now.setHours(now.getHours() + 3);
+  // now.setHours(now.getHours() + 3);
 
   const date = {
-    year: now.getUTCFullYear(),
-    month: now.getUTCMonth() + 1,
-    day: now.getUTCDate(),
-    now,
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    day: now.getDate(),
   };
 
   return date;
